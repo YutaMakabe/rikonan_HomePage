43ca1349a329185ca364023b5da54b024d4bbb86
Yuta-Irisawa <iryusawa1284@gmail.com>
2021-02-26 12:30:50 +0900

    [UPD] Update schedule

--- js/demo.js
+++ js/demo.js
@@ -3,8 +3,6 @@
 var events = [
     {'Date': new Date(2020, 11, 23), 'Title': '練習'},
     {'Date': new Date(2021, 0, 7), 'Title': '紅白戦'},
-    {'Date': new Date(2021, 1, 24), 'Title': '練習'},
-    {'Date': new Date(2021, 2, 3), 'Title': '練習'},
     {'Date': new Date(2021, 2, 10), 'Title': '練習'},
     {'Date': new Date(2021, 2, 17), 'Title': '練習'},
     {'Date': new Date(2021, 2, 19), 'Title': '練習試合'},
@@ -14,6 +12,7 @@ var events = [
     {'Date': new Date(2021, 2, 27), 'Title': '公式戦'},
     {'Date': new Date(2021, 2, 29), 'Title': '練習'},
     {'Date': new Date(2021, 2, 31), 'Title': '練習'},
+    {'Date': new Date(2021, 3, 4), 'Title': '練習'},
     {'Date': new Date(2021, 3, 7), 'Title': '練習'},
     {'Date': new Date(2021, 3, 10), 'Title': '練習'},
     {'Date': new Date(2021, 3, 14), 'Title': '練習'},
@@ -23,7 +22,7 @@ var events = [
     {'Date': new Date(2021, 4, 1), 'Title': '公式戦'},
     {'Date': new Date(2021, 4, 2), 'Title': '公式戦'},
 ];
-  
+
 var settings = {};
 var element = document.getElementById('caleandar');
 caleandar(element, events, settings);
